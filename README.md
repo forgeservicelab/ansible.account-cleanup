82db03ad6b276e6775520095f1cc859a789a07f0
Jukka Nousiainen <jukka.nousiainen@digile.fi>
2015-04-14 13:24:23 +0300

    Some sanity checks for variables

--- README.md
+++ README.md
@@ -24,7 +24,7 @@ Usage
 -----
 
 ```
-$ ./clenaup.sh [optional ansible parameters]
+$ ./cleanup.sh [optional ansible parameters]
 ```
 
 Author Information
--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
+#Note! All arguments are passed to ansible-playbook at the end of the script.
+
+if grep -qE 'username\:[[:space:]]*$|api_key\:[[:space:]]*$|project_id\:[[:space:]]*$' nova.yml
+then
+    echo "Configuration incomplete in nova.yml"
+    exit
+fi
+
+if grep -E '\[\]|\[[[:space:]]*\]' group_vars/all
+then
+    echo "No accounts configured for removal in group_vars/all"
+    exit
+fi
+
 ANSIBLE_NOVA_CONFIG=nova.yml ansible-playbook -e ansible_ssh_user=`whoami` -i inventory cleanup.yml "$@"
